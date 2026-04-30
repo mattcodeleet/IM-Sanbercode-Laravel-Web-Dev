52a1b0b271cb2975deffbdb1446cf035bf14b7b1
mattcodeleet <setiawanrachmat62@gmail.com>
2026-04-30 10:29:02 +0700

    Tugas 13 – Memasangkan Template dengan Laravel Blade

--- inventoryapp_rachmat/resources/views/home.blade.php
+++ inventoryapp_rachmat/resources/views/home.blade.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ini Laravel</title>
-</head>
-<body>
-    <h1>SanberBook</h1>
+@extends('layout.master')
+@section('title')
+    Home
+@endsection
+@section('content')
     <h2>Social Media Developer Santai Berkualitas</h2>
     <p>Belajar dan Berbagi agar hidup ini semakin santai berkualitas</p>
     <h3>Benefit Join di SanberBook</h3>
@@ -21,5 +17,4 @@
         <li>Mendaftar di <a href="/register">Form Sign Up</a></li>
         <li>Selesai!</li>
     </ol>
-</body>
-</html>
+@endsection
--- inventoryapp_rachmat/resources/views/pages/register.blade.php
+++ inventoryapp_rachmat/resources/views/pages/register.blade.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ini Laravel</title>
-</head>
-<body>
-    <h1>Buat account Baru!</h1>
-    <h3>Sign Up Form</h3>
+@extends('layout.master')
+@section('title')
+    Register
+@endsection
+@section('content')
     <form action="/welcome" method="POST">
         @csrf
         <label>First Name:</label><br><br>
@@ -15,26 +10,24 @@
         <label>Last Name:</label><br><br>
         <input type="text" name="last_name" required><br><br>
         <label>Gender:</label><br><br>
-        <input type="radio" name="gen" value="m">Male<br>
-        <input type="radio" name="gen" value="f">Female<br>
-        <input type="radio" name="gen" value="oth">Other<br><br>
+        <input type="radio" name="gen" value="m"> Male<br>
+        <input type="radio" name="gen" value="f"> Female<br>
+        <input type="radio" name="gen" value="oth"> Other<br><br>
         <label>Nationality:</label><br><br>
         <select name="nation">
-            <option value="indo">Inonesian</option>
+            <option value="indo">Indonesian</option>
             <option value="singa">Singaporean</option>
             <option value="malay">Malaysian</option>
             <option value="aus">Australian</option>
             <option value="oth">Other</option>
         </select><br><br>
         <label>Language Spoken:</label><br><br>
-        <input type="checkbox" name="lang" value="indo">Bahasa Indonesia<br>
-        <input type="checkbox" name="lang" value="eng">English<br>
-        <input type="checkbox" name="lang" value="oth">Other<br><br>
+        <input type="checkbox" name="lang" value="indo"> Bahasa Indonesia<br>
+        <input type="checkbox" name="lang" value="eng"> English<br>
+        <input type="checkbox" name="lang" value="oth"> Other<br><br>
         <label>Bio:</label><br><br>
-        <textarea name="bio" cols="30" rows="10" required></textarea><br>
-        <a href="/"><button type="button">Kembali</button></a>
-        <input type="submit" value="Sign Up">
-        
+        <textarea name="bio" cols="30" rows="10" required></textarea><br>        
+        <input type="submit" value="Sign Up">      
+        <p>Belum siap? Silahkan <a href="/">kembali</a></p>
     </form>
-</body>
-</html>
+@endsection    
